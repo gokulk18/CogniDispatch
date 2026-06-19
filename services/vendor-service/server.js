b62dc2bafb4052eebdf9c2aab63869be6704cc85
gokulk18 <gokulkakhilesh@gmail.com>
2026-06-19 11:07:31 +0530

    ci: trigger rebuild of vendor-service to generate semantic tag

--- services/vendor-service/server.js
+++ services/vendor-service/server.js
@@ -15,3 +15,5 @@ const PORT = process.env.PORT || 5002;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Vendor Service listening on port ${PORT}`);
 });
+
+// Trigger rebuild for semantic tag update
